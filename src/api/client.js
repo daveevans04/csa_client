b90b0e5b87a5d976583553776ce0d485f2ccef79
Evans <48053069+daveevans04@users.noreply.github.com>
2023-06-22 15:12:43 +0800

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://192.168.100.209:4848/api', // Replace with your API base URL
+  baseURL: 'https://computer-system-architecture.onrender.com/api', // Replace with your API base URL
   headers: {
     'Cache-Control': 'no-cache', // Add the Cache-Control header
   },
